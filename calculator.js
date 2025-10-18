39a825afc6c5f1bf92fcaecbf125663f7a29ae32
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 21:24:28 +0100

    Added simple calculator file

---
+++ calculator.js
@@ -0,0 +1,60 @@
+function add (number1,number2){
+ return number1 + number2
+}
+
+function subtract (number1,number2){
+ return number1 - number2
+}
+
+function multiply (number1,number2){
+ return number1 * number2
+}
+
+function divide (number1,number2){
+ return number1 / number2
+}
+
+
+
+const add = document.getElementById("add").addEventListener(('click'), function(){
+  const number1 = parseFloat(document.getElementById("number1").value) || 0
+  const number2 = parseFloat(document.getElementById("number2").value) || 0
+
+  const result = add(number1,number2)
+  document.getElementById("calculation-resul").textContent = result
+
+
+})
+ 
+const add = document.getElementById("subtract").addEventListener(('click'), function(){
+  const number1 = parseFloat(document.getElementById("number1").value) || 0
+  const number2 = parseFloat(document.getElementById("number2").value) || 0
+
+  const result = subtract(number1,number2)
+  document.getElementById("calculation-resul").textContent = result
+
+
+})
+ 
+
+const add = document.getElementById("multiply").addEventListener(('click'), function(){
+  const number1 = parseFloat(document.getElementById("number1").value) || 0
+  const number2 = parseFloat(document.getElementById("number2").value) || 0
+
+  const result = multiply(number1,number2)
+  document.getElementById("calculation-resul").textContent = result
+
+
+})
+
+const add = document.getElementById("divide").addEventListener(('click'), function(){
+  const number1 = parseFloat(document.getElementById("number1").value) || 0
+  const number2 = parseFloat(document.getElementById("number2").value) || 0
+
+  const result = divide(number1,number2)
+  document.getElementById("calculation-resul").textContent = result
+
+
+})
+ 
+ 
