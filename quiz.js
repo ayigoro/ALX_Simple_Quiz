f07356b381b9dcd23623ba53a7fae19b6699b4b6
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 20:32:55 +0100

    Added quiz.js file and others to the simple quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+  const correctAnswer = '4'
+  userAnswer = document.querySelector('input[name="quiz"]:checked').value
+  const feedback = document.getElementById("feedback")
+  if (correctAnswer === userAnswer){
+    feedback.textContent = "Correct! Well done."
+  }
+  else if (correctAnswer =! userAnswer){
+    feedback.textContent = " That's incorrect. Try again!"
+  }
+}
+
+const submitAnswer = document.getElementById("submit-answer")
+submitAnswer.addEventListener(('click'),function{
+  checkAnswer()
+})
